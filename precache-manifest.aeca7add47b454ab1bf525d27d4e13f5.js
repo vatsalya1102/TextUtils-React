13d8ef7928653139ca51f6bdd1f4d7a25fe1ffff
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2022-11-10 15:18:02 +0530

    Updates

---
+++ precache-manifest.aeca7add47b454ab1bf525d27d4e13f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16a1bfd5b0d7a8f1b5b5",
+    "url": "/TextUtils-React/static/css/main.3d42e5fc.chunk.css"
+  },
+  {
+    "revision": "16a1bfd5b0d7a8f1b5b5",
+    "url": "/TextUtils-React/static/js/main.16a1bfd5.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/TextUtils-React/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtils-React/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "7c72d7100d14b77fea39",
+    "url": "/TextUtils-React/static/js/runtime~main.7c72d710.js"
+  },
+  {
+    "revision": "773549906f5f0fbe358bc5f76fcd4082",
+    "url": "/TextUtils-React/index.html"
+  }
+];
